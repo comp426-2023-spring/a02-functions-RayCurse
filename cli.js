1330f54b07c72256c5bebb2aa3c224fdef0eafb3
RayCurse <38709018+RayCurse@users.noreply.github.com>
2023-02-21 12:55:38 -0500

    Add output

--- cli.js
+++ cli.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
+import minimist from 'minimist';
+import moment from 'moment-timezone';
+import fetch from 'node-fetch';
+
 // Help message
-const minimist = require("minimist")
 const args = minimist(process.argv)
 if (args.h) {
     console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
@@ -31,10 +34,10 @@ if ("e" in args && "w" in args) {
     console.log("ERROR: Cannot specify LONGITUDE twice.")
     process.exit(1)
 }
-if ((!("n" in args) && !("s" in args)) || (!("e" in args) && !("w" in args))) {
-    console.error("ERROR: Must specify both LATITUDE and LONGITUDE.")
-    process.exit(1)
-}
+// if ((!("n" in args) && !("s" in args)) || (!("e" in args) && !("w" in args))) {
+//     console.error("ERROR: Must specify both LATITUDE and LONGITUDE.")
+//     process.exit(1)
+// }
 if ("d" in args && (args.d < 0 || args.d > 6)) {
     console.error("ERROR: Day option -d must be 0-6.")
     process.exit(1)
@@ -43,6 +46,28 @@ if ("d" in args && (args.d < 0 || args.d > 6)) {
 // Get user input
 const latitude = "n" in args ? args.n : -args.s
 const longitude = "e" in args ? args.e : -args.w
-const timezone = "z" in args ? args.z : require("moment-timezone").tz.guess()
+const timezone = "z" in args ? args.z : moment.tz.guess()
 const day = "d" in args ? args.d : 1
 const json = "j" in args
+
+// Make request to Open_Mateo
+const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=precipitation_hours`)
+const data = await response.json()
+
+// Write output
+if (json) {
+    console.log(data)
+    process.exit(0)
+}
+if (data.daily.precipitation_hours[day] <= 0) {
+    process.stdout.write("You will not need your galoshes ");
+} else {
+    process.stdout.write("You might need your galoshes ");
+}
+if (day == 0) {
+    console.log("today.")
+} else if (day == 1) {
+    console.log("tomorrow.")
+} else {
+    console.log(`in ${day} days.`)
+}
