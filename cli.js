fe87a66367298762224a2eac6bdfcf293e1712d2
RayCurse <38709018+RayCurse@users.noreply.github.com>
2023-02-21 12:33:32 -0500

    Get user input

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+// Help message
 const minimist = require("minimist")
 const args = minimist(process.argv)
 if (args.h) {
@@ -12,3 +13,36 @@ if (args.h) {
     -j            Echo pretty JSON from open-meteo API and exit.`)
     process.exit(0)
 }
+
+// Validate input
+for (let k in args) {
+    let validArgs = "hnsewzdj"
+    if (!validArgs.includes(k)) {
+        if (k == "_") { continue }
+        console.error(`ERROR: Invalid option: -${k}.`)
+        process.exit(1)
+    }
+}
+if ("n" in args && "s" in args) {
+    console.error("ERROR: Cannot specify LATITUDE twice.")
+    process.exit(1)
+}
+if ("e" in args && "w" in args) {
+    console.log("ERROR: Cannot specify LONGITUDE twice.")
+    process.exit(1)
+}
+if ((!("n" in args) && !("s" in args)) || (!("e" in args) && !("w" in args))) {
+    console.error("ERROR: Must specify both LATITUDE and LONGITUDE.")
+    process.exit(1)
+}
+if ("d" in args && (args.d < 0 || args.d > 6)) {
+    console.error("ERROR: Day option -d must be 0-6.")
+    process.exit(1)
+}
+
+// Get user input
+const latitude = "n" in args ? args.n : -args.s
+const longitude = "e" in args ? args.e : -args.w
+const timezone = "z" in args ? args.z : require("moment-timezone").tz.guess()
+const day = "d" in args ? args.d : 1
+const json = "j" in args
